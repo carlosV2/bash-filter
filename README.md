09caea46343ac19c0407d56fa3c99ae8a6b49d9d
Carlos Ortega Huetos <carlos@rechannel.co.uk>
2016-05-17 14:40:22 +0100

    Add the morphologic derivatives metacharacter

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 bash-filter
 ===========
 
-This script provides the missing filter functionality for bash pipes.
+This script provides the missing filter functionality for bash.
 
 Installation
 ============
@@ -20,24 +20,12 @@ $ echo "source $(pwd)/.bash-filter/filter.sh" >> ~/.bash_profile
 Usage
 =====
 
-Once you have installed this script, you have the function `filter` available to be used as a **pipe**.
+Once you have installed this script, you then have the function `filter` available to be used as a **pipe**.
 This function reads the standard input line by line and applies the specified filters to it. The output
 will be all those lines that match the given filters.
 
-Filters are defined as regular parameter string. For example, the string `abc` would match any text that
-contains `abc`. They are also case insensitive so `abc`, `Abc`, `aBc`, `ABC`, etc match among them.
-
-There are also metacharacters to modify the behaviour of each filter:
-- **+filter**: The `+` metacharacter states that `filter` must be contained for the line to match it.
-- **-filter**: The `-` metacharacter states that `filter` must not be contained for the line to match it.
-- **=filter**: The `=` metacharacter states that `filter` must be a word for the line to match it (i.e.
-surrounded by spaces)
-
-Metacharacters can be stacked together having in mind that `=` metacharacter needs to precede the others.
-For example, the filter `=-word` would match any line without `word` but `-=word` would much any line that
-does not contain `=word`.
-
-By default, if no `+` or `-` metacharacter is provided, the `+` metacharacter is applied automatically.
+Filters are defined as regular parameter strings. For example, the string `foo` would match any text that
+contains `foo`. They are also case insensitive so `foo`, `Foo`, `fOo`, `FOO`, etc match among them.
 
 This command appends some stats at the end. Those stats contains:
 - The number of matching lines
@@ -60,13 +48,71 @@ $ echo "export FILTER_STATS=false" >> ~/.bash_profile
 $ echo "export FILTER_COLORS=false" >> ~/.bash_profile
 ```
 
+
++ metacharacter:
+----------------
+
+The plus (+) metacharacter states that the given filter **must be** contained in the line to match it.
+
+For example, the filter `+foo` will match a line if there is any occurrence of `foo` in it.
+
+
+- metacharacter:
+----------------
+
+The minus (-) metacharacter states that the given filter **must not be** contained in the line to match it.
+
+For example, the filter `-foo` will match a line if there are not occurrences of `foo` in it.
+
+
+~ metacharacter:
+----------------
+
+The tilde (~) metacharacter states that the given filter should be searched as itself and any morphologic
+derivative of it.
+
+For example, the filter `~foo` will be searching for occurrences of `foo` and `foos`.
+
+Note: Currently, this functionality is limited to the English language and singular/plural words. It is also
+recommended to input the word in the singular form for a better performance. However, plural words may also
+work. If you find an issue with this functionality, please, don't hesitate to open an issue.
+
+
+= metacharacter:
+----------------
+
+The equals (=) metacharacter states that the given filter should be searched as a standalone word to match it.
+
+For example, the filter `=foo` will match a line only if `foo` is it preceded and followed by spaces.
+
+
+Combining metacharacters:
+-------------------------
+
+Metacharacters can be combined to form more accurate filters.
+However, the combination order must be as follows:
+
+1. =
+2. ~
+3. +/-
+
+Examples:
+- `=-foo`: Will match a line if `foo` is not contained in as a standalone word (it might be contained as part of
+bigger words, though).
+- `=~foo`: Will match a line if either `foo` or `foos` are contained as words.
+- `~-foo`: Will match a line if neither `foo` or `foos` are contained in it. 
+- `+~foo`: Because the combination order is broken, this filter will match a line if it contains `~foo` in it (being
+`~` a regular character and not a metacharacter) 
+
+By default, if no `+` or `-` metacharacter is provided, the `+` metacharacter is applied automatically.
+
+
 Examples
 ========
 
 Imagine we have the following file:
 ```
 $ cat bestsellers.txt
-Book - Author
 Fantastic Beasts and Where to Find Them - J. K. Rowling
 Harry Potter and the Cursed Child - J. K. Rowling
 The Girl on the Train - Paula Hawkins
@@ -77,9 +123,10 @@ The World's Worst Children - David Walliams
 The Road to Little Dribbling - Bill Bryson
 Career of Evil - Robert Galbraith
 Hitman Anders and the Meaning of it All - Jonas Jonasson
+Lost Girls - Angela Marsons
 ```
 
-We can filter the books that contain "the" in the name:
+We can filter the books that contain `the`:
 ```
 $ cat bestsellers.txt | filter the
 Fantastic Beasts and Where to Find Them - J. K. Rowling
@@ -89,8 +136,9 @@ The Reader on the 6.27 - Jean-Paul Didierlaurent
 The Girl of Ink & Stars - Kiran Millwood-Hargrave
 The World's Worst Children - David Walliams
 The Road to Little Dribbling - Bill Bryson
+Hitman Anders and the Meaning of it All - Jonas Jonasson
 
-Found 7 of 10 (0 blank lines)
+Found 8 of 11 (0 blank lines)
 ```
 
 Alternatively, the `+` metacharacter can be applied with the same result:
@@ -98,7 +146,7 @@ Alternatively, the `+` metacharacter can be applied with the same result:
 $ cat bestsellers.txt | filter +the
 ```
 
-This filter is matching "them" too but we only want to match the word "the":
+This filter is matching `them` too but we only want to match the word `the`:
 ```
 $ cat bestsellers.txt | filter =the
 Harry Potter and the Cursed Child - J. K. Rowling
@@ -107,11 +155,12 @@ The Reader on the 6.27 - Jean-Paul Didierlaurent
 The Girl of Ink & Stars - Kiran Millwood-Hargrave
 The World's Worst Children - David Walliams
 The Road to Little Dribbling - Bill Bryson
+Hitman Anders and the Meaning of it All - Jonas Jonasson
 
-Found 6 of 10 (0 blank lines)
+Found 7 of 11 (0 blank lines)
 ```
 
-Finally, if we don't want to match any book from J. K. Rowling, we can use:
+If we don't want to match any book from J. K. Rowling, we can use:
 ```
 $ cat bestsellers.txt | filter =the -rowling
 The Girl on the Train - Paula Hawkins
@@ -119,9 +168,17 @@ The Reader on the 6.27 - Jean-Paul Didierlaurent
 The Girl of Ink & Stars - Kiran Millwood-Hargrave
 The World's Worst Children - David Walliams
 The Road to Little Dribbling - Bill Bryson
+Hitman Anders and the Meaning of it All - Jonas Jonasson
 
-Found 5 of 10 (0 blank lines)
+Found 6 of 11 (0 blank lines)
 ```
 
-You can use `filter` in combination with any other command that writes to the standard
-output: `cat`, `find`, `apt-get`, etc  
+Finally, if we want to match any book about girls, we can use:
+```
+$ cat bestsellers.txt | filter ~girl
+The Girl on the Train - Paula Hawkins
+The Girl of Ink & Stars - Kiran Millwood-Hargrave
+Lost Girls - Angela Marsons
+
+Found 3 of 11 (0 blank lines)
+```
--- filter.sh
+++ filter.sh
@@ -1,17 +1,147 @@
 function filter()
 {
-    GREEN="\033[32m"
-    YELLOW="\033[33m"
-    RESET="\033[0m"
+    # Based on https://github.com/ICanBoogie/Inflector project
+    function singularify()
+    {
+        word=`echo "${1}" | tr "[A-Z]" "[a-z]"`
 
-    filters=""
-    for arg in "$@"
-    do
+        regexes=(
+            # Irregulars
+            's/^leaves$/leaf/g'
+            's/^loaves$/loaf/g'
+            's/^octopuses$/octopus/g'
+            's/^viruses$/virus/g'
+            's/^people$/person/g'
+            's/^men$/man/g'
+            's/^children$/child/g'
+            's/^sexes$/sex/g'
+            's/^moves$/move/g'
+            's/^zombies$/zombie/g'
+            's/^geese$/goose/g'
+
+            # Rules
+            's/(database)s$/\1/g'
+            's/(quiz)zes$/\1/g'
+            's/(matr)ices$/\1ix/g'
+            's/(vert|ind)ices$/\1ex/g'
+            's/^(ox)en/\1/g'
+            's/(alias|status)(es)?$/\1/g'
+            's/^(a)x[ie]s$/\1xis/g'
+            's/(cris|test)(is|es)$/\1is/g'
+            's/(shoe)s$/\1/g'
+            's/(o)es$/\1/g'
+            's/(bus)(es)?$/\1/g'
+            's/^(m|l)ice$/\1ouse/g'
+            's/(x|ch|ss|sh)es$/\1/g'
+            's/(m)ovies$/\1ovie/g'
+            's/(s)eries$/\1eries/g'
+            's/([^aeiouy]|qu)ies$/\1y/g'
+            's/([lr])ves$/\1f/g'
+            's/(tive)s$/\1/g'
+            's/(hive)s$/\1/g'
+            's/([^f])ves$/\1fe/g'
+            's/(^analy)(sis|ses)$/\1sis/g'
+            's/((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)(sis|ses)$/\1sis/g'
+            's/([ti])a$/\1um/g'
+            's/(n)ews$/\1ews/g'
+            's/(ss)$/\1/g'
+            's/s$//g'
+        )
+
+        uncountables=" advice art coal baggage butter clothing cotton currency equipment experience feedback fish flour food furniture gas homework impatience information jeans knowledge leather love luggage money oil management patience police polish progress research rice series sheep staff silk soap species sugar talent toothpaste travel vinegar weather wood wool work "
+        uncountable=`echo "${uncountables}" | grep " ${word} "`
+
+        if [ "${uncountable}" != "" ]; then
+            echo "${word}"
+            return
+        fi
+
+        for regex in "${regexes[@]}"; do
+            singular=`echo "${word}" | sed -E "${regex}"`
+            if [ "${singular}" != "${word}" ]; then
+                echo "${singular}"
+                return
+            fi
+        done
+
+        echo "${word}"
+    }
+
+    # Based on https://github.com/ICanBoogie/Inflector project
+    function pluralify()
+    {
+        word=`echo "${1}" | tr "[A-Z]" "[a-z]"`
+
+        regexes=(
+            # Irregulars
+            's/^leaves$/leaf/g'
+            's/^loaves$/loaf/g'
+            's/^octopuses$/octopus/g'
+            's/^viruses$/virus/g'
+            's/^people$/person/g'
+            's/^men$/man/g'
+            's/^children$/child/g'
+            's/^sexes$/sex/g'
+            's/^moves$/move/g'
+            's/^zombies$/zombie/g'
+            's/^geese$/goose/g'
+
+            # Rules
+            's/(quiz)$/\1zes/g'
+            's/^(oxen)$/\1/g'
+            's/^(ox)$/\1en/g'
+            's/^(m|l)ice$/\1ice/g'
+            's/^(m|l)ouse$/\1ice/g'
+            's/(matr|vert|ind)(ix|ex)$/\1ices/g'
+            's/(x|ch|ss|sh)$/\1es/g'
+            's/([^aeiouy]|qu)y$/\1ies/g'
+            's/(hive)$/\1s/g'
+            's/(([^f])fe|([lr])f)$/\2\3ves/g'
+            's/sis$/ses/g'
+            's/([ti])a$/\1a/g'
+            's/([ti])um$/\1a/g'
+            's/(buffal|tomat)o$/\1oes/g'
+            's/(bu)s$/\1ses/g'
+            's/(alias|status)$/\1es/g'
+            's/^(ax|test)is$/\1es/g'
+            's/s$/s/g'
+            's/$/s/g'
+        )
+
+        uncountables=" advice art coal baggage butter clothing cotton currency equipment experience feedback fish flour food furniture gas homework impatience information jeans knowledge leather love luggage money oil management patience police polish progress research rice series sheep staff silk soap species sugar talent toothpaste travel vinegar weather wood wool work "
+        uncountable=`echo "${uncountables}" | grep " ${word} "`
+
+        if [ "${uncountable}" != "" ]; then
+            echo "${word}"
+            return
+        fi
+
+        for regex in "${regexes[@]}"; do
+            plural=`echo "${word}" | sed -E "${regex}"`
+            if [ "${plural}" != "${word}" ]; then
+                echo "${plural}"
+                return
+            fi
+        done
+
+        echo "${word}"
+    }
+
+    function filterify()
+    {
+        tildeFlag=false
+        separator=""
         param=""
-        search="${arg}"
+        search="${1}"
 
         if [ "${search:0:1}" == "=" ]; then
             search="${search:1}"
+            separator=" "
+        fi
+
+        if [ "${search:0:1}" == "~" ]; then
+            search="${search:1}"
+            tildeFlag=true
         fi
 
         if [ "${search:0:1}" == "-" ]; then
@@ -21,40 +151,62 @@ function filter()
             search="${search:1}"
         fi
 
-        if [ "${arg:0:1}" == "=" ]; then
-            search=" ${search} "
+        if [ "${tildeFlag}" == true ]; then
+            singular=`singularify "${search}"`
+            plural=`pluralify "${search}"`
+
+            search="${singular}${separator}\|${separator}${plural}"
         fi
 
-        filters="${filters} | grep ${param} -i -- \"${search}\""
-    done
-    
-    total=0
-    found=0
-    blank=0
-    while read -r line; do
-        if [[ "${line}" == "" ]]; then
-            blank=$((blank + 1))
-        else
-            result=`eval "echo ' ${line//\'/\'\"'\"'} ' ${filters}"`
+        echo " | grep ${param} -i -- \"${separator}${search}${separator}\""
+    }
 
-            if [[ "${result}" != "" ]]; then
-                echo "${line}"
-                found=$((found + 1))
+    function main()
+    {
+        GREEN="\033[32m"
+        YELLOW="\033[33m"
+        RESET="\033[0m"
+
+        filters=""
+        for arg in "$@"
+        do
+            filters="${filters} `filterify ${arg}`"
+        done
+        
+        total=0
+        found=0
+        blank=0
+        while read -r line; do
+            if [[ "${line}" == "" ]]; then
+                blank=$((blank + 1))
+            else
+                result=`eval "echo ' ${line//\'/\'\"'\"'} ' ${filters}"`
+
+                if [[ "${result}" != "" ]]; then
+                    echo "${line}"
+                    found=$((found + 1))
+                fi
             fi
+            total=$((total + 1))
+        done
+
+        if [[ ${FILTER_STATS} != false ]]; then
+            blank="${blank} blank lines"
+
+            if [[ ${FILTER_COLORS} != false ]]; then
+                total="${GREEN}${total}${RESET}"
+                found="${GREEN}${found}${RESET}"
+                blank="${YELLOW}${blank}${RESET}"
+            fi
+
+            echo
+            echo -e "Found ${found} of ${total} (${blank})"
         fi
-        total=$((total + 1))
-    done
+    }
 
-    if [[ ${FILTER_STATS} != false ]]; then
-        blank="${blank} blank lines"
+    main "$@"
 
-        if [[ ${FILTER_COLORS} != false ]]; then
-            total="${GREEN}${total}${RESET}"
-            found="${GREEN}${found}${RESET}"
-            blank="${YELLOW}${blank}${RESET}"
-        fi
-
-        echo
-        echo -e "Found ${found} of ${total} (${blank})"
-    fi
+    unset -f singularify
+    unset -f pluralify
+    unset -f filterify
 }
